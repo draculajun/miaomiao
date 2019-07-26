a148be5046b6f0f87e374d8853b442229dda923d
王文俊 <wangwenjun@wangwenjundeMacBook-Air.local>
2019-07-26 13:53:07 +0800

    1

---
+++ src/routers/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import movieRouter from './movie/movie_router'
+import cinemaRouter from './cinema/cinema_router'
+import mineRouter from './mine/mine_router'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    movieRouter,
+    cinemaRouter,
+    mineRouter
+  ]
+})
---
+++ src/views/movie/movie_view.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Hello movie
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
