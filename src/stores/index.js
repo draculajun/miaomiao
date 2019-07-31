e8128b3dd17c9f788407084ecd02fe73e7f4b56d
wangwj <wenjun.wang@athub.com>
2019-07-31 13:51:35 +0800

    1

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,20 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 import person from './person/person_store'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [createPersistedState({})],
   modules: {
     person
-  },
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
   },
 })
